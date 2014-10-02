7149b5c5ab31f7471a8707606431c1827d2059ea
Diogo <dggf.93@gmail.com>
2014-10-02 12:37:03 +1000

    coding print statements

--- python2perl.pl
+++ python2perl.pl
@@ -5,28 +5,64 @@
 #Written by Diogo G. Garcia de Freitas (dggf.93@gmail.com) September 2014
 
 
+sub translateLine #Contains whole translation algorithm
+{
+	my $line = $_[0]; #Variable $line gets the attribute passed as parameter
+	
+	if ($line =~ /^#!/ && $. == 1)
+	{
+		#Includes the #! line
 
-while ($line = <>) {
-	if ($line =~ /^#!/ && $. == 1) {
-	
-		# translate #! line 
-		
 		print "#!/usr/bin/perl -w\n";
-	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-	
-		# Blank & comment lines can be passed unchanged
-		
-		print $line;
-	} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
-	
-		# Python's print print a new-line character by default
-		# so we need to add it explicitly to the Perl print statement
-		
+	}
+	elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/)
+	{
+		#Blank and comment lines can be passed unchanged
+
+		print "$line";
+	}
+	elsif ($line =~ /^\s*print\s*"(.*)"\s*$/)
+	{
+		#Python's print print a new-line character by default, so we need to add it explicitly to the Perl print statement		
+
 		print "print \"$1\\n\";\n";
-	} else {
-	
-		# Lines we can't translate are turned into comments
-		
+	}
+	elsif ($line =~ /^['"].*['"]/) #If not a string 
+	{
+		#Translates variable names
+
+		@temp = $line =~ m/[a-zA-Z0-9]+/g;
+		push(@variables, @temp);
+
+		foreach $variable (@variables)
+		{
+			#print "\$$variable";
+		}
+	}
+	else
+	{
+		#Lines we can't translate are turned into comments
 		print "#$line\n";
 	}
+}
+
+
+if ($#ARGV + 1 == 0) #Read from stdin 	
+{
+	while ($l = <>)
+	{
+		#translateLine($l);
+	}	
+}
+elsif ($#ARGV + 1 == 1) #Open and read from file
+{
+	open(F,"<$ARGV[0]") or die "$0: Can't open $ARGV[0]: $!\n";
+	
+	while(<F>)
+	{	
+		#print "$_\n"; #debugging
+		translateLine($_);
+	}
+
+	close(F);
 }
